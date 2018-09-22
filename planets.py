bf762b48197fd1a0a14ed17de7f5c2c7ef31e35f
ashingtondc <ashingtondc@gmail.com>
2018-09-22 12:17:39 -0700

    Passed test

--- planets.py
+++ planets.py
@@ -1,6 +1,13 @@
 def weight_on_planets():
    # write your code here
    
+   #ask for weight, store, and convert
+   earthWeight = float(input('What do you weigh on earth? '))
+   marsWeight = earthWeight * 0.38
+   jupiterWeight = earthWeight * 2.34
+   
+   #print weights
+   print("\nOn Mars you would weigh", round(marsWeight, 3), "pounds.\nOn Jupiter you would weigh", round(jupiterWeight, 3), "pounds.")
    
    
 if __name__ == '__main__':
